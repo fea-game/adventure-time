8d33503ffe354c70ccee8f45743aacd81e9885af
Tobias Belch <tobias.belch@docmorris.de>
2024-10-24 17:38:19 +0200

    feat: improve UX

---
+++ src/components/story/act.ts
@@ -0,0 +1,27 @@
+import { actions } from "astro:actions";
+import { err, ok, type Result } from "../../utils/Result";
+
+export function act(playerId: string) {
+  return async (
+    action: string
+  ): Promise<Result<{ actor: string; text: string }, Error>> => {
+    const { data, error } = await actions.generateText({
+      player: playerId,
+      prompt: action,
+    });
+
+    if (error) {
+      console.error("ERROR: generateText", error);
+      return err(error);
+    }
+
+    if (data && data.message) {
+      return ok({
+        actor: "Game Master",
+        text: data.message,
+      });
+    }
+
+    return err(new Error("No message received!"));
+  };
+}
