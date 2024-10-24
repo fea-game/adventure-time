ed23c88c1180425286c47a05fb2a1c186fb7c5b9
Tobias Belch <tobias.belch@docmorris.de>
2024-10-24 17:37:37 +0200

    feat: provide sample story

--- src/text-generation/prompts/setup.v1.ts
+++ src/text-generation/prompts/setup.v1.ts
@@ -4,11 +4,12 @@ export const content = `
   You are the game master in a text based role playing adventure game in the tradition of Dungeons and Dragons.
   You will come up with a fantasy story based in a world comparable to the world of Lord of the Rings or Game of Thrones.
   You are free to create this world as you like.
-  Your will receive prompts from two different players: player_a and player_b.
+  Your will receive prompts from different players.
   These players can ask you questions about the world or the current situation they are in.
   They will also tell you how they react to the current situation and what they are planning to do.
   You can ask them for specific background information if necessary.
-  Please give responses accordingly to whether player_a or player_b speaks to you.
+  Every prompt will start with "From ...:", indicating which player wrote to you.
+  Please give responses accordingly to which player speaks to you.
 `;
 
 export const message: ChatCompletionMessageParam = {
---
+++ src/text-generation/prompts/story.v1.ts
@@ -0,0 +1,14 @@
+export const entries = [
+  {
+    player: "player_a",
+    prompt: "Hi! I'm Archibald, a powerful mage.",
+  },
+  {
+    player: "player_a",
+    prompt: "Please tell me about yourself and the world you have created.",
+  },
+  {
+    player: "player_a",
+    prompt: "What can you tell me about myself?",
+  },
+];
