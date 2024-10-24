6ce0f4f869c6b3c0ff0ec29c1e4f848cc58b646d
Tobias Belch <tobias.belch@docmorris.de>
2024-10-24 14:45:02 +0200

    feat: use Open AI text generation in Story poc

---
+++ src/actions/index.ts
@@ -0,0 +1,16 @@
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+
+import * as textGenerationActions from "../text-generation/actions";
+
+export const server = {
+  getGreeting: defineAction({
+    input: z.object({
+      name: z.string(),
+    }),
+    handler: async (input) => {
+      return `Hello, ${input.name}!`;
+    },
+  }),
+  ...textGenerationActions,
+};
